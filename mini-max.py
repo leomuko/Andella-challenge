443e2d15092193837ac73260b674359a66cd8ddc
leomuko <kezzieleo@gmail.com>
2018-11-21 08:12:55 +0300

    Error handling

--- mini-max.py
+++ mini-max.py
@@ -1,12 +1,16 @@
 def mini_max():
-    a, b, c, d, e = raw_input('Enter 5 spaced integers: ').split()
-    a, b, c, d, e = [int(a),int(b),int(c),int(d),int(e)]
-    numbers = [a,b,c,d,e]
-    numbers.sort()
-    max = sum(numbers[1:])
-    min = sum(numbers[:(len(numbers)- 1)])
-    final = [str(min), str(max)]
-    return ' '.join(final)
+    try:
+        a, b, c, d, e = raw_input('Enter 5 spaced integers: ').split()
+        a, b, c, d, e = [int(a),int(b),int(c),int(d),int(e)]    
+        numbers = [a,b,c,d,e]
+        numbers.sort()
+        max = sum(numbers[1:])
+        min = sum(numbers[:(len(numbers)- 1)])
+        final = [str(min), str(max)]
+        return ' '.join(final)
+    except ValueError:
+        print('Please enter 5 variables and ensure they are spaced')
+        return     
 
 
 print(mini_max())
